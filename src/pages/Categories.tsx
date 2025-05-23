9e534e524820c7f7306a9397288f005def7f6310
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2025-05-23 16:23:41 +0530

    add react router

---
+++ src/pages/Categories.tsx
@@ -0,0 +1,11 @@
+
+function Categories() {
+
+  return (
+    <>
+     <body style={{fontSize:'50px', justifyContent: 'center'}}>Categories Page</body>
+    </>
+  )
+}
+
+export default Categories
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+     <body style={{fontSize:'50px', justifyContent: 'center'}}>Home Page</body>
+    </>
+  )
+}
+
+export default App
