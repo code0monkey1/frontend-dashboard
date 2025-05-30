0f338471a0002c8e246ce3f93d7512fe62056e7b
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2025-05-30 11:58:38 +0530

    add input validation using ant design

--- src/pages/login/login.spec.tsx
+++ src/pages/login/login.spec.tsx
@@ -1,16 +1,16 @@
 import Login from "./login.tsx"
-import{describe, it, expect} from 'vitest'
-import { render,screen } from "@testing-library/react"
-
+import { describe, it, expect } from 'vitest'
+import { render, screen } from "@testing-library/react"
+import '@testing-library/jest-dom'
 
 describe('login', () => {
+  
     it('should render login page', () => {
         render(<Login/>)
-        expect(screen.getByText('Login Page')).toBeInTheDocument()
-        expect(screen.getByPlaceholderText('UserName')).toBeInTheDocument()
+        expect(screen.getByText('Sign In')).toBeInTheDocument()
+        expect(screen.getByPlaceholderText('User Name')).toBeInTheDocument()
         expect(screen.getByPlaceholderText('Password')).toBeInTheDocument()
-
-        expect(screen.getByRole('button',{name:"Log In"})).toBeInTheDocument()
-        expect(screen.getByRole('checkbox',{name:"Remember Me"})).toBeInTheDocument()
+        expect(screen.getByRole('button', { name: 'Submit' })).toBeInTheDocument()
+        expect(screen.getByRole('checkbox', { name: 'Remember me' })).toBeInTheDocument()
     })
 });
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,5 +1,6 @@
 import { Button, Card, Checkbox, Input, Layout, Space,Form, Typography } from "antd"
 import { LockFilled ,LockOutlined, UserOutlined} from '@ant-design/icons';
+import Logo from "../../components/icons/Logo";
 
 function Login() {
 
@@ -9,10 +10,7 @@ function Login() {
 
       <Space size='small' direction="vertical">
         <Layout.Content style={{ display:"flex",justifyContent:"center",alignItems:"center"}}>
-          <svg width="100" height="40" viewBox="0 0 100 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M39.746 9.432C39.746 10.164 39.578 10.836 39.242 11.448C38.906 12.048 38.39 12.534 37.694 12.906C36.998 13.278 36.134 13.464 35.102 13.464H33.194V18H30.116V5.364H35.102C36.11 5.364 36.962 5.538 37.658 5.886C38.354 6.234 38.876 6.714 39.224 7.326C39.572 7.938 39.746 8.64 39.746 9.432ZM34.868 11.016C35.456 11.016 35.894 10.878 36.182 10.602C36.47 10.326 36.614 9.936 36.614 9.432C36.614 8.928 36.47 8.538 36.182 8.262C35.894 7.986 35.456 7.848 34.868 7.848H33.194V11.016H34.868ZM44.4264 5.364V18H41.3484V5.364H44.4264ZM49.933 15.48H55.369V18H46.441V15.66L51.841 7.884H46.441V5.364H55.369V7.704L49.933 15.48ZM60.6557 15.48H66.0917V18H57.1637V15.66L62.5637 7.884H57.1637V5.364H66.0917V7.704L60.6557 15.48ZM75.9683 15.768H71.2523L70.4963 18H67.2743L71.8463 5.364H75.4103L79.9823 18H76.7243L75.9683 15.768ZM75.1763 13.392L73.6103 8.766L72.0623 13.392H75.1763Z" fill="#484848"/>
-            <circle cx="11" cy="11" r="7.5" stroke="#F65F42" stroke-width="7"/>
-          </svg>
+         <Logo/>
         </Layout.Content>
       <Card title={<Space style={{width:"100%",fontSize:16 ,justifyContent:"center"}}>
         <LockFilled/> Sign In</Space>
@@ -30,7 +28,8 @@ function Login() {
         <Form.Item
           name="username"
         
-          rules={[{ required: true, message: 'Please input your username!' }]}
+          rules={[{ required: true, message: 'Please input your username!' },  
+            { type:"email" , message:'The username should be an email'}]}
         >
           <Input placeholder="User Name" prefix={<UserOutlined/>} />
         </Form.Item>
@@ -38,7 +37,9 @@ function Login() {
         <Form.Item
         
           name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
+          rules={[
+            { required: true, message: 'Please input your password!' }
+          ]}
         >
           <Input.Password prefix={<LockOutlined/>} placeholder="Password" />
         </Form.Item>
@@ -46,9 +47,10 @@ function Login() {
         <Form.Item name="remember" valuePropName="checked" noStyle>
           
           <Checkbox>Remember me</Checkbox>
-          <Typography.Link href="/"  > Forgot Password </Typography.Link>
+
        
         </Form.Item>
+         <Typography.Link href="/"  > Forgot Password </Typography.Link>
 
           <Form.Item style={{marginTop: 24, width: "100%"}}>
             <Button type="primary" htmlType="submit" size="large" style={{width: "100%"}}>
